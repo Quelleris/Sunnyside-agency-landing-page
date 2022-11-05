4b7e85521bb7be30850d6c6a001cb88a748056e5
Quelleris <74406524+Quelleris@users.noreply.github.com>
2022-11-05 18:31:36 +0100

    disable scrolling while mobile navbar opened

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
 const navbar = document.querySelector('[data-navbar]');
 const navToggle = document.querySelector('.nav-mobile-toggle');
 const header = document.querySelector('[data-header]');
+const body = document.querySelector('body');
 
 navToggle.addEventListener('click', () => {
     if (navbar.classList.contains('active')) {
         navToggle.classList.remove('active');
         navbar.classList.remove('active');
+        body.classList.remove('body-fixed');
     } else {
         navToggle.classList.add('active');
         navbar.classList.add('active');
+        body.classList.add('body-fixed');
     }
 })
 
